14e7930bfbf3d1678624ec419db17df14b42c003
AKalicka1985 <akalicka1985@gmail.com>
2025-10-30 12:29:08 +0100

    Add files via upload

---
+++ calculator2.py
@@ -0,0 +1,20 @@
+a = int(input("podaj a: "))
+b = int(input("podaj b: "))
+operator = input("wybierz znak: ")
+
+tabela_liczb=[1,2,3,4,5,6,7,8,9,0]
+try:
+    a and b != tabela_liczb
+except Exception as e:
+    print ("Wartość nie jest liczbą: " + e)
+
+poprawny_operator = int["+","-","*","/","^","%"]
+try:
+    operator != poprawny_operator
+except Exception as e:
+    print ("Zastosowano niepoprawny operator: " + e)
+
+try:
+    b/0 and a/0
+except Exception as e:
+    print ("Dzielenie przez 0 jest niepoprawną operacją: " + e)
